910ac7e7daa65dac848e013e150152b730915436
Matthew Maynes <matthew.maynes@yahoo.ca>
2016-09-18 10:40:29 -0400

    Added ctrl-serve skeleton

---
+++ ctrl-serve/src/example.py
@@ -0,0 +1,6 @@
+
+class Example ():
+
+    def __init__ (self):
+        self.name = 'example'
+
---
+++ ctrl-serve/test/test_example.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+class Example (TestCase):
+
+    def test_example (self):
+        self.assertEquals('foo'.upper(), 'FOO')
