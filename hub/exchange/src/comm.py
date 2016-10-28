3b7e39f672f33aa06ea60146711d1b65f670a07c
Jeremy Dunsmore <jeremydunsmore@cmail.carleton.ca>
2016-10-28 13:52:32 -0400

    add comm

---
+++ hub/exchange/src/comm.py
@@ -0,0 +1,32 @@
+"""
+Abstract class used for communicating with devices
+using a specific protocol
+"""
+class Comm:
+
+    """
+    bind to port and any other setup required to
+    communicate with devices
+
+    """
+    def setup(self) : pass
+
+    """
+    scan network for any new devices
+    """
+    def discover() : pass
+
+    """
+    send a message to a device
+    """
+    def send (device, message) : pass
+
+    """
+    close connections and unbind from port
+    """
+    def teardown() : pass
+
+    """
+    listen for messages from devices
+    """
+    def receive(): pass
