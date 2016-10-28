d07528ad4f5ee5df0600543a8beca804814f91ee
peter-mark <peter_mark12@hotmail.com>
2016-10-28 14:11:05 -0400

    trying to get tests working

--- hub/gateway/test/example.spec.js
+++ hub/gateway/test/example.spec.js
@@ -1,8 +1,25 @@
 var packets = require('../src/ccp')
+   // buffer = require('buffer').Buffer
 
 describe('Parse packet', function () {
 
-    it('Should return an empty string', function () {
-        expect(packets.parse()).toBe('');
+    it('Should return an object representation of packet', function () {
+        var expectedStructure = {
+            type: 1,
+            size: 4,
+            sender: 16,
+            destination: 16,
+            payload: 0
+        }
+
+        const buf = Buffer.from ([1,
+                                0, 0, 0, 4,
+                                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16,
+                                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0, 0, 0, 16,
+                                0, 0, 0, 0 ]);
+
+        expect(packets.parse(buf)).toBe(expectedStructure);
     });
 });
+
+
