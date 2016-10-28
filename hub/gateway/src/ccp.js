3fc49dc3d413d79b7a2eda9cf6f3c6b8e288471d
ctblanch2 <ctblanch@gmail.com>
2016-10-28 15:35:22 -0400

    Gateway packet parse complete

--- hub/gateway/src/ccp.js
+++ hub/gateway/src/ccp.js
@@ -1,9 +1,11 @@
+var uuid = require('node-uuid')
+
 module.exports.parse = function (buffer) {
     var type = buffer.readUIntBE(0, 1)
     var size = buffer.readUIntBE(1, 4)
-    var sender = buffer.readUIntBE(5, 16)
-    var destination = buffer.readUIntBE(21, 16)
-    var payload = buffer.readUIntBE(37, size)
+    var sender = buffer.slice(5, 21)
+    var destination = buffer.slice(21, 37)
+    var payload = JSON.parse(buffer.toString('utf8', 37))
 
     return {
         type: type,
--- hub/gateway/test/ccptests.js
+++ hub/gateway/test/ccptests.js
@@ -1,25 +1,44 @@
 var packets = require('../src/ccp')
 var expect = require('chai').expect
+var uuid = require('node-uuid')
 
 describe('Parse packet', function () {
 
     it('Should return an object representation of packet', function () {
+        var senderUUIDBuf = new Buffer(16)
+        var destinationUUIDBuf = new Buffer(16)
+
+        uuid.v4(null, senderUUIDBuf, 0)
+        uuid.v4(null, destinationUUIDBuf, 0)
+
         var expectedStructure = {
             type: 1,
-            size: 4,
-            sender: 16,
-            destination: 16,
-            payload: 0
+            size: 23,
+            sender: senderUUIDBuf,
+            destination: destinationUUIDBuf,
+            payload: {
+                version : "0.0.2"
+            }
         }
 
-        const buf = Buffer.from ([0x01,
-                                0, 0, 0, 0x04,
-                                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x10,
-                                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0, 0, 0, 0x10,
-                                0, 0, 0, 0 ]);
+        var payloadString = '{ "version" : "0.0.2" }'
+
+        var payloadLengthB = new Buffer(4)
+        payloadLengthB.fill(0)
+        payloadLengthB.writeUInt32BE(payloadString.length, 0)
+
+        const buf = Buffer.concat ([Buffer.from([0x01]),
+                                   payloadLengthB,
+                                   senderUUIDBuf,
+                                   destinationUUIDBuf,
+                                   Buffer.from(payloadString)]);
 
         expect(packets.parse(buf)).to.deep.equal(expectedStructure);
     });
 });
+
+describe('Serialize packet', function() {
+
+})
 
 
