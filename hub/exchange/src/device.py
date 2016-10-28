1b672f0178c205ec8129cc712b487065f3d940ca
Matthew Maynes <matthew.maynes@yahoo.ca>
2016-10-28 14:15:57 -0400

    Added hub communicator

--- hub/exchange/src/device.py
+++ hub/exchange/src/device.py
@@ -2,6 +2,7 @@ class Device:
 
     default = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
 
-    def __init__ (self, name = '', address = default ):
+    def __init__ (self, type_, name = '', address = default ):
+        self.type    = type_
         self.name    = name
         self.address = address
--- hub/exchange/src/exchange.py
+++ hub/exchange/src/exchange.py
@@ -2,10 +2,22 @@ class Exchange ():
 
     def __init__ (self):
         self.comms = {}
-
+        self.devices = {}
 
     def register (self, device_type, comm):
         self.comms[device_type] = comm
         comm.setup(self)
 
-    def message (device, message): pass
+
+    def send (self, device, message):
+        # TODO Log sending a message here
+        if (device.type in self.comms):
+            self.comms[device.type](message)
+
+    def message (self, msg):
+        # TODO Log a received message here
+        if (msg.target in self.devices):
+            self.send(msg)
+
+    def discovered (self, device):
+        self.devices[device.address] = device
