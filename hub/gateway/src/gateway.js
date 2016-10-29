e3be822b40448146254c0e0909ac7aead19ffe61
ctblanch2 <ctblanch@gmail.com>
2016-10-29 11:10:12 -0400

    Added discover message to gateway

--- hub/gateway/src/gateway.js
+++ hub/gateway/src/gateway.js
@@ -1,49 +1,60 @@
 const dgram = require('dgram')
 const packets = require('./ccp')
 const uuid = require('node-uuid')
+const config = require('../config.json')
+
+var senderId = new Buffer(16)
+uuid.v4(null, senderId, 16)
+
+var registerWithCommServer = function(){
+    sendMessageToCommServer(1, {}, function(message){
+    })
+}
+
+var sendMessageToCommServer = function(type, payload, responseCallback){
+    var client = dgram.createSocket('udp4')
+
+    var packet = {
+        type: type,
+        sender: senderId,
+        destination: new Buffer(16).fill(0),
+        payload: payload
+    }
+
+    message = packets.serialize(packet)
+
+    client.on('message', function(message, remote){
+        console.log('Received response from comm server')
+        client.close()
+
+        responseCallback(message)
+    })
+
+    var timeoutCallback = function() {
+        client.close()
+        responseCallback('')
+        console.log("CCP udp status request timed out")
+    }
+
+    setTimeout(timeoutCallback, 5000)
+
+    client.send(message, 0, message.length, 7600, config.communicationServer, function(err, bytes) {
+        if (err) throw err
+        console.log("Sent UDP message")
+    })
+}
 
 module.exports.gateway= function(expressApp){
 
     expressApp.get('/system/state', function(req, res) {
 
-        var client = dgram.createSocket('udp4')
-
-        var senderBuffer = new Buffer(16)
-        uuid.v4(null, senderBuffer, 16)
-
-        var packet = {
-            type: 1,
-            sender: senderBuffer,
-            destination: new Buffer(16).fill(0),
-            payload: {'message' : 'test'}
-        }
-
-        message = packets.serialize(packet)
-
-        client.on('message', function(message, remote){
-            console.log('received message')
-            client.close()
-
-            response = packets.parse(message)
-            res.send(JSON.stringify({
-                'version' : response.version
-            }))
-        })
-
-        var timeoutCallback = function() {
-            client.close()
-            res.status = 500
-            res.end()
-            console.log("CCP udp status request timed out")
-        }
-
-        setTimeout(timeoutCallback, 5000)
-
-        client.send(message, 0, message.length, 7600, 'localhost', function(err, bytes) {
-            if (err) throw err
-            console.log("Sent UDP message")
+        sendMessageToCommServer(2, {'message': 'test'}, function(response){
+            res.send(response)
         })
     })
+
+    registerWithCommServer()
 }
+
 
 
