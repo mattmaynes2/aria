f43ccd922cba1d3fa7e17a3baaacf233633a86a7
Jeremy Dunsmore <jeremydunsmore@cmail.carleton.ca>
2016-11-10 18:50:51 -0500

    add tests for device

---
+++ hub/exchange/test/device/test_device.py
@@ -0,0 +1,18 @@
+import uuid
+import json
+from unittest import TestCase
+from device import Device
+
+
+class DeviceTest(TestCase):
+    
+    def setUp (self):
+        self.id=uuid.uuid4()        
+        self.dev=Device(type_='wemo', name= 'Switch', address=self.id.bytes)
+    
+    def test_str(self):
+        self.assertEqual('Device [type: wemo, name: Switch, address: '+str(self.id)+']',str(self.dev))
+    
+    def test_encode(self):
+        expected='{"address": "'+str(self.id)+'", "name": "Switch", "type": "wemo"}'
+        self.assertEqual(json.dumps(self.dev,default=Device.encode,sort_keys=True), expected)
