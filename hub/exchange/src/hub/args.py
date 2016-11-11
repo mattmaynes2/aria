2f12b3ac144b4d259e8260ca5551b0ef58528879
Jeremy Dunsmore <jeremydunsmore@cmail.carleton.ca>
2016-11-11 12:48:30 -0500

    get logging working for console and file

--- hub/exchange/src/hub/args.py
+++ hub/exchange/src/hub/args.py
@@ -14,5 +14,5 @@ def parse ():
     )
     parser.add_argument('--debug', nargs='?', const=True, help='Display debugging information when running')
     parser.add_argument('-d', '--daemonize', nargs='?', const=True, help='Fork this process into the background')
-
+    parser.add_argument('-f', '--file',type=str, help='Log to output file specified')
     return parser.parse_args()
--- hub/exchange/src/hub/cli.py
+++ hub/exchange/src/hub/cli.py
@@ -1,14 +1,16 @@
+import logging
 from threading import Thread
 
+logger=logging.getLogger(__name__)
+formatter=logging.Formatter('%(asctime)s %(threadName)s %(name)-12s %(levelname)-8s %(message)s')
+
 class CLI (Thread):
-    LEVEL_EMERGENCY = 'FATAL'
-    LEVEL_ALERT     = 'ALERT'
-    LEVEL_CRITICAL  = 'CRITICAL'
-    LEVEL_ERROR     = 'ERROR'
-    LEVEL_WARN      = 'WARN'
-    LEVEL_NOTIFY    = 'NOTIFY'
-    LEVEL_INFO      = 'INFO'
-    LEVEL_DEBUG     = 'DEBUG'
+    LEVEL_EMERGENCY = logging.FATAL
+    LEVEL_CRITICAL  = logging.CRITICAL
+    LEVEL_ERROR     = logging.ERROR
+    LEVEL_WARN      = logging.WARN
+    LEVEL_INFO      = logging.INFO
+    LEVEL_DEBUG     = logging.DEBUG
     PROMPT          = ''
     COMMANDS        = {
         'status'    : 'Returns the system status',
@@ -16,11 +18,24 @@ class CLI (Thread):
         'exit'      : 'exit the main server'
     }
 
-    def __init__ (self, listener, level = LEVEL_WARN):
+    def __init__ (self, listener, level = LEVEL_WARN, filename=None):
         super().__init__()
-        self.level    = level
+        logger.setLevel(level)
+        ch = logging.StreamHandler()
+        print('setting level to '+str(level))
+        ch.setLevel(level)
+        ch.setFormatter(formatter)
+        # add to root logger
+        logging.getLogger('').addHandler(ch)
         self.listener = listener
         self.running  = True
+        
+        if(filename):
+            fh=logging.FileHandler(filename)
+            # log everything to file
+            fh.setLevel(logging.DEBUG)
+            fh.setFormatter(formatter)
+            logging.getLogger('').addHandler(fh)
 
     def command (self):
         cmd = input(CLI.PROMPT)
@@ -38,8 +53,7 @@ class CLI (Thread):
 
 
     def log (self, msg, level = LEVEL_INFO):
-        if (self.level >= level):
-            print('[' + level + '] ' + msg + CLI.PROMPT)
+        logger.log(level,msg)
 
     def help (self):
         print('Commands')
