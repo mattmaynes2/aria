b935ec91f9b8d7f5e958db28b160e8307c99d5f8
ctblanch2 <ctblanch@gmail.com>
2016-11-13 11:29:38 -0500

    Added self-pipe to shut down aria adapter cleanly

--- hub/exchange/src/adapter/aria_adapter.py
+++ hub/exchange/src/adapter/aria_adapter.py
@@ -1,4 +1,6 @@
 import socket
+import os
+import select
 
 from .message import Message
 from .adapter import Adapter
@@ -15,6 +17,7 @@ class AriaAdapter (Adapter):
         self.socket     = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.port       = AriaAdapter.PORT
         self._ip_map    = {}
+        self.self_rd, self.self_wd = os.pipe()
 
     def setup (self ):
         super().setup()
@@ -28,6 +31,7 @@ class AriaAdapter (Adapter):
 
     def teardown (self):
         super().teardown()
+        os.write(self.self_wd, bytes('x', 'utf-8'))
         self.socket.close()
         return True
 
@@ -46,21 +50,27 @@ class AriaAdapter (Adapter):
 
     def receive (self):
         print('Listening')
-        chunk, address = self.socket.recvfrom(AriaAdapter.BUFFER_SIZE)
-        #while chunk:
-        #   data.append(chunk)
-        #   chunk = self.socket.recv(AriaAdapter.BUFFER_SIZE)
-        #payload = ''.join(data)
-        payload = chunk
+       
+        readables, writeables, exceptions = select.select([self.self_rd, self.socket.fileno()], [], [])
+        if (self.socket.fileno() in readables):
+            chunk, address = self.socket.recvfrom(AriaAdapter.BUFFER_SIZE)
+            #while chunk:
+            #   data.append(chunk)
+            #   chunk = self.socket.recv(AriaAdapter.BUFFER_SIZE)
+            #payload = ''.join(data)
+            payload = chunk
 
-        print('Received all data')
-        msg = Message.decode(payload)
-        self._ip_map[msg.sender] = address
+            print('Received all data')
+            msg = Message.decode(payload)
+            self._ip_map[msg.sender] = address
 
-        if (msg.type == Message.Discover):
-            self.notify('discovered', Device('aria', '', msg.sender))
-            self.notify('received', Message(Message.Ack, '', msg.receiver, msg.sender))
+            if (msg.type == Message.Discover):
+                self.notify('discovered', Device('aria', '', msg.sender))
+                self.notify('received', Message(Message.Ack, '', msg.receiver, msg.sender))
+            else:
+                self.notify('received', msg)
         else:
-            self.notify('received', msg)
+            os.close(self.self_rd)
+            os.close(self.self_wd)
 
         return True
