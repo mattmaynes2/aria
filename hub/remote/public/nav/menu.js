6bfbf575b4d6c4b14a330d2fcb4c710ba0ed2c19
Matthew Maynes <matthew.maynes@yahoo.ca>
2016-11-19 19:12:59 -0500

    Added ARIA title to nav-bar

--- hub/remote/public/nav/menu.js
+++ hub/remote/public/nav/menu.js
@@ -24,13 +24,17 @@ let PAGES = [
 class Menu extends Component {
     constructor () {
         super();
-        this._$menuGroup = null;
-        this._$menuItems = [];
-    }
-    render () {
-        this._$menuGroup = $('<div>').addClass('nav-menu-group');
-        this._$el.addClass('nav-menu').append(this._$menuGroup);
+        this._state = {
+            title   : 'ARIA'
+        };
 
+        this._$menuGroup    = null;
+        this._$menuItems    = [];
+    }
+
+    render () {
+        this._$title = $('<div>').addClass('nav-title').text(this._state.title);
+        this._$menuGroup = $('<div>').addClass('nav-menu-group');
         this._$menuGroup.append(PAGES.map((page) => {
             var $item = $('<div>')
                 .addClass('nav-menu-item')
@@ -39,6 +43,8 @@ class Menu extends Component {
             this._$menuItems.push($item);
             return $item;
         }));
+
+        this._$el.addClass('nav-menu').append(this._$title).append(this._$menuGroup);
 
         return this;
     }
