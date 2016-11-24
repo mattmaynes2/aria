f6cb7d1ddaadff5b97b165f57b3ff8cb0be5b216
Jeremy Dunsmore <jeremydunsmore@cmail.carleton.ca>
2016-11-24 15:30:26 -0500

    fix syntax for integration tests

--- hub/exchange/test/integration/Database.py
+++ hub/exchange/test/integration/Database.py
@@ -6,7 +6,7 @@ from device     import DeviceType
 from ipc import Message
 from adapter import Adapter
 from database import Database
-from database import RequestTracker
+from delegate import RequestTracker
 import queue
 import sqlite3
 import os
@@ -37,9 +37,9 @@ class TestDatabaseIntegration(TestCase):
         except OSError as e:
             pass
 
-        self.hub         = Hub()
-        self.cli         = CLI(self.hub)
         self.database    = Database(self._testMethodName + ".db")
+        self.hub         = Hub(self.database)
+        self.cli         = CLI(self.hub)
         self.exchange    = Exchange(self.hub, self.cli, self.database)
         self.testAdapter = StubDeviceAdapter()
         self.exchange.register('stub', self.testAdapter)
