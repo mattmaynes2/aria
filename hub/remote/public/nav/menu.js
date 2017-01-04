697baeebf709fd520bc4239ccf1ee83355b74186
Matthew Maynes <matthew.maynes@yahoo.ca>
2017-01-04 11:39:00 -0500

    Added new logo to navigation bar

--- hub/remote/public/nav/menu.js
+++ hub/remote/public/nav/menu.js
@@ -24,16 +24,12 @@ let PAGES = [
 class Menu extends Component {
     constructor () {
         super();
-        this._state = {
-            title   : 'ARIA'
-        };
-
         this._$menuGroup    = null;
         this._$menuItems    = [];
     }
 
     render () {
-        this._$title = $('<div>').addClass('nav-title').text(this._state.title);
+        this._$title = $('<div>').addClass('nav-title');
         this._$menuGroup = $('<div>').addClass('nav-menu-group');
         this._$menuGroup.append(PAGES.map((page) => {
             var $item = $('<div>')
