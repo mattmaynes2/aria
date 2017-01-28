147589701c7c3a84776eac1ffe56201bb2716cab
Matthew Maynes <matthew.maynes@yahoo.ca>
2017-01-27 19:56:40 -0500

    Added notofication history to notify

--- hub/remote/public/core/notify/notify.js
+++ hub/remote/public/core/notify/notify.js
@@ -18,17 +18,29 @@ toastr.options = {
   'hideMethod'        : 'fadeOut'
 };
 
+var history = [];
+
 class Notify {
+    static get history () {
+        return history;
+    }
+    static reset () {
+        history = [];
+    }
     static info (text) {
+        history.push({ type : 'info', text : text });
         toastr.info(text);
     }
     static warn (text) {
+        history.push({ type : 'warn', text : text});
         toastr.warning(text);
     }
     static success (text) {
+        history.push({ type : 'success', text : text });
         toastr.success(text);
     }
     static error (text) {
+        history.push({ type : 'error', text : text});
         toastr.error(text);
     }
 }
