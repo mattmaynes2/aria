09c3c6b3f71b9859d5196198cbcc1eac90b4a9fe
ctblanch2 <ctblanch@gmail.com>
2017-03-03 11:01:33 -0500

    Refactor Strategy V1

--- hub/exchange/src/brain/strategies/v1_strategy.py
+++ hub/exchange/src/brain/strategies/v1_strategy.py
@@ -1,8 +1,18 @@
 
 class V1Strategy():
 
-    def __init__(self, triggeredEvent):
-        self.triggeredEvent = triggeredEvent
+    def __init__(self):
+        self.triggeredEvent = None
+
+    def processSession(self, events):
+        for event in reversed(events):
+            if event['request_id']:
+                message = Message(Message.Request, data=
+                {
+                    'set':event['attribute_name'],
+                    'value':[{'name':event['parameter_name'], 'value':event['value']}]
+                }, receiver=UUID(event['source']).bytes)
+                self.triggeredEvent = message
 
     def decide(self, event):
         return self.triggeredEvent
