47da2a9c379fe5b3be300f0ef5c2dd86486e8aa6
Jeremy Dunsmore <jeremydunsmore@cmail.carleton.ca>
2017-03-03 19:53:32 -0500

    add command to delete behaviours\r\ncloses#630

--- hub/exchange/src/__main__.py
+++ hub/exchange/src/__main__.py
@@ -15,7 +15,8 @@ from database import Database, Retriever
 from ipc import Message
 from device     import SoftwareDeviceFactory
 from hub.commands import GetDeviceEventsCommand,GetEventWindowCommand,GetBehavioursCommand,\
- CreateBehavioursCommand, CreateSessionCommand, ActivateSessionCommand, DeactivateSessionCommand
+ CreateBehavioursCommand, CreateSessionCommand, ActivateSessionCommand, DeactivateSessionCommand,\
+ DeleteBehaviourCommand
 from brain.model_builder import ModelBuilder
 from brain.decision_broker import DecisionBroker
 from brain.strategies import V2Strategy
@@ -102,6 +103,7 @@ def setupCommands(hub,database):
     hub.addCommand(CreateSessionCommand(database))
     hub.addCommand(ActivateSessionCommand(database))
     hub.addCommand(DeactivateSessionCommand(database))
+    hub.addCommand(DeleteBehaviourCommand(database))
 
 
 if (__name__ == '__main__'):
